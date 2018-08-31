5e259b5b3a153beb31e78edab27e358034801e8e
hernss <hernantravado@yahoo.com.ar>
2018-08-31 16:34:27 -0300

    Add wallet distribution by version

--- main.cpp
+++ main.cpp
@@ -297,6 +297,8 @@ extern "C" void* ThreadDumper(void*) {
     Sleep(100000 << count); // First 100s, than 200s, 400s, 800s, 1600s, and then 3200s forever
     if (count < 5) count++;
     {
+		std::map<string, int> versionCount;
+
       vector<CAddrReport> v = db.GetAll();
       sort(v.begin(), v.end(), StatCompare);
       FILE *f = fopen("/usr/local/share/dnsseed/dnsseed.dat.new","w+");
@@ -310,20 +312,40 @@ extern "C" void* ThreadDumper(void*) {
       FILE *d = fopen("/usr/local/share/dnsseed/dnsseed.dump", "w");
       fprintf(d, "# address                                        good  lastSuccess    %%(2h)   %%(8h)   %%(1d)   %%(7d)  %%(30d)  blocks      svcs  version\n");
       double stat[5]={0,0,0,0,0};
-      for (vector<CAddrReport>::const_iterator it = v.begin(); it < v.end(); it++) {
-        CAddrReport rep = *it;
-        fprintf(d, "%-47s  %4d  %11"PRId64"  %6.2f%% %6.2f%% %6.2f%% %6.2f%% %6.2f%%  %6i  %08"PRIx64"  %5i \"%s\"\n", rep.ip.ToString().c_str(), (int)rep.fGood, rep.lastSuccess, 100.0*rep.uptime[0], 100.0*rep.uptime[1], 100.0*rep.uptime[2], 100.0*rep.uptime[3], 100.0*rep.uptime[4], rep.blocks, rep.services, rep.clientVersion, rep.clientSubVersion.c_str());
-		//fprintf(d, "%s,%d,%d,%6.2f%%,%6.2f%%,%6.2f%%,%6.2f%%,%6.2f%%,%i,%d,%5i %s\n", rep.ip.ToString().c_str(), (int)rep.fGood, rep.lastSuccess, 100.0*rep.uptime[0], 100.0*rep.uptime[1], 100.0*rep.uptime[2], 100.0*rep.uptime[3], 100.0*rep.uptime[4], rep.blocks, rep.services, rep.clientVersion, rep.clientSubVersion.c_str());  //csv
-        stat[0] += rep.uptime[0];
-        stat[1] += rep.uptime[1];
-        stat[2] += rep.uptime[2];
-        stat[3] += rep.uptime[3];
-        stat[4] += rep.uptime[4];
+	  for (vector<CAddrReport>::const_iterator it = v.begin(); it < v.end(); it++) {
+		  CAddrReport rep = *it;
+		  fprintf(d, "%-47s  %4d  %11"PRId64"  %6.2f%% %6.2f%% %6.2f%% %6.2f%% %6.2f%%  %6i  %08"PRIx64"  %5i \"%s\"\n", rep.ip.ToString().c_str(), (int)rep.fGood, rep.lastSuccess, 100.0*rep.uptime[0], 100.0*rep.uptime[1], 100.0*rep.uptime[2], 100.0*rep.uptime[3], 100.0*rep.uptime[4], rep.blocks, rep.services, rep.clientVersion, rep.clientSubVersion.c_str());
+		  //fprintf(d, "%s,%d,%d,%6.2f%%,%6.2f%%,%6.2f%%,%6.2f%%,%6.2f%%,%i,%d,%5i %s\n", rep.ip.ToString().c_str(), (int)rep.fGood, rep.lastSuccess, 100.0*rep.uptime[0], 100.0*rep.uptime[1], 100.0*rep.uptime[2], 100.0*rep.uptime[3], 100.0*rep.uptime[4], rep.blocks, rep.services, rep.clientVersion, rep.clientSubVersion.c_str());  //csv
+		  stat[0] += rep.uptime[0];
+		  stat[1] += rep.uptime[1];
+		  stat[2] += rep.uptime[2];
+		  stat[3] += rep.uptime[3];
+		  stat[4] += rep.uptime[4];
+		  if (versionCount.count(rep.clientSubVersion)) {
+			  std::map<string, int>::iterator it2 = versionCount.find(rep.clientSubVersion);
+			  (*it2).second += 1;
+		  }
+		  else {
+			  versionCount[rep.clientSubVersion] = 1;
+		  }
       }
       fclose(d);
       FILE *ff = fopen("/usr/local/share/dnsseed/dnsstats.log", "a");
       fprintf(ff, "%llu %g %g %g %g %g\n", (unsigned long long)(time(NULL)), stat[0], stat[1], stat[2], stat[3], stat[4]);
       fclose(ff);
+
+	  FILE *fff = fopen("/usr/local/share/dnsseed/wallets.json", "w");
+	  fprintf(fff, "{");
+	  std::map<string, int>::iterator it2 = versionCount.begin();
+	  while (it2 != versionCount.end()) {
+			
+		  fprintf(fff, "\"%s\":%i", (*it2).first.c_str(), (*it2).second);
+
+		  it2++;
+		  if(it2 != versionCount.end()) fprintf(fff, ",");
+	  }
+	  fprintf(fff, "}");
+	  fclose(fff);
     }
   } while(1);
 }
